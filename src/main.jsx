58570afe983dfd73d51969773458213ed0e78922
Shahed <sk0178397@gmial.com>
2023-12-21 01:48:26 +0600

    Created structure file.

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainLayout from "./MainLayout.jsx";
+import Home from "./Home.jsx";
+import SignIn from "./SignIn.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: "/signIn",
+        element: <SignIn></SignIn>,
+      },
+      {
+        path: "/signUp",
+        element: <SignIn></SignIn>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
