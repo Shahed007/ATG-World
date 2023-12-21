b3a9e85adebb929def3ea4bb403c4b8bedfb0423
Shahed <sk0178397@gmial.com>
2023-12-21 13:35:31 +0600

    Created hero section.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+import { MdArrowBack } from "react-icons/md";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <section className="hero-section">
+        <div className="container h-100 ">
+          <div className="pt-5 w-100 d-flex d-sm-none  justify-content-between align-items-center">
+            <Link to="/" className="text-light" style={{ fontSize: "3rem" }}>
+              <MdArrowBack />
+            </Link>
+            <button
+              type="button"
+              className="btn btn-outline-light px-4 py-3 join-btn-outline"
+            >
+              Join Group
+            </button>
+          </div>
+          <div className="d-flex flex-column justify-content-end align-items-start h-100 hero-section-text-box">
+            <h1 className="text-light ">Computer Engineering</h1>
+            <h4 className="text-light">
+              142,765 Computer Engineers follow this
+            </h4>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,10 +1,12 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 const MainLayout = () => {
   return (
-    <div>
+    <>
       <Navbar></Navbar>
-    </div>
+      <Outlet></Outlet>
+    </>
   );
 };
 
